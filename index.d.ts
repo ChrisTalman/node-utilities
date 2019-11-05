83488e2132a25ca68899be99aa60e34e42bef885
Chris Talman <cewe.mail@gmail.com>
2019-11-05 16:41:01 +0000

    Prepared for NPM.

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
-declare module '@ChrisTalman/node-utilities'
+declare module '@chris-talman/node-utilities'
 {
 	/** Initialises handler to be invoked when process receives signals relevant to a graceful exit. */
 	export function initialiseGracefulExitHandler(handleGracefulExit: () => void): void;
+
 	/** Generates hash for value according to encoding. */
 	import { HexBase64Latin1Encoding as DigestEncoding } from 'crypto';
 	export function generateHash({value, algorithm, encoding}: {value: string, algorithm: 'sha256', encoding: DigestEncoding}): string;
+
 	/** Listens to unhandled exceptions and rejections, logs them, and then exits with error code `1`. */
 	export function listenUnhandledErrors(): void;
 }
