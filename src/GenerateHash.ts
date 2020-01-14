8f127297ee4002de0e1c4fe4b79c24acaabf3434
Chris Talman <cewe.mail@gmail.com>
2020-01-14 11:58:40 +0000

    Allowed algorithm 'sha1' for basic unsecure hashes.

--- src/GenerateHash.ts
+++ src/GenerateHash.ts
@@ -7,7 +7,7 @@ import { createHash } from 'crypto';
 import { HexBase64Latin1Encoding as DigestEncoding } from 'crypto';
 
 /** Generates hash for value according to encoding. */
-export function generateHash({value, algorithm, encoding}: {value: string, algorithm: 'sha256', encoding: DigestEncoding})
+export function generateHash({value, algorithm, encoding}: {value: string, algorithm: 'sha1' | 'sha256', encoding: DigestEncoding})
 {
     const hasher = createHash(algorithm);
     hasher.update(value);
